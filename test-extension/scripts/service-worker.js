7c0f300554019050c9f1975a5750557b67be16c2
Mitch <69921413+tensesailor@users.noreply.github.com>
2025-01-20 16:40:01 -0800

    current project to repo

---
+++ test-extension/scripts/service-worker.js
@@ -0,0 +1,9 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        console.log("onBeforeRequest", details.url);
+
+    },
+    {
+        urls: ["https://nhcares.alayacare.com/#/builder"]
+    }
+  )
