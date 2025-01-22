4e469fc1cc80d44d5274df9e690624ae61fc2d57
Mitch <69921413+tensesailor@users.noreply.github.com>
2025-01-21 16:59:28 -0800

    blah

---
+++ test-extension/contentscripts/builder.js
@@ -0,0 +1,33 @@
+console.log("builder loaded");
+
+document.addEventListener("DOMContentLoaded", function() {
+    const domObserver = new MutationObserver((_mutationList, observer) => {
+        const nav = document.querySelector("div.sideNav");
+        
+        if (nav) {
+            domObserver.disconnect();
+            btn = document.createElement("li");
+            btn.setAttribute("data-v-52b1fb15", true);
+            btn.setAttribute("data-acl", "schedule/admin");
+            nav.firstElementChild.appendChild(btn);
+    
+            link = document.createElement("a");
+            link.setAttribute("data-v-024a2701", true);
+            link.setAttribute("data-v-52b1fb15", true);
+            //link.setAttribute("href", "#/builder");
+            btn.appendChild(link);
+    
+            img = document.createElement ("i")
+            img.setAttribute("data-v-52b1fb15", true);
+            img.setAttribute("class", "ph-calendar-blank");
+            link.appendChild(img);
+    
+            title = document.createElement("span");
+            title.setAttribute("data-v-52b1fb15", true);
+            title.innerText = "WIP";
+            link.appendChild(title);
+         }
+    });
+    
+    domObserver.observe(document.body, {childList: true, subtree: true});
+});
