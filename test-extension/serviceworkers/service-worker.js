3932644b85d0384dfa2aeff11d04b238971bc690
Mitch <69921413+tensesailor@users.noreply.github.com>
2025-01-22 16:57:52 -0800

    asdf

--- test-extension/serviceworkers/service-worker.js
+++ test-extension/serviceworkers/service-worker.js
@@ -1,10 +1,29 @@
-console.log("Service worker initiated");
-
-browser.webRequest.onHeadersReceived.addListener(
+browser.webRequest.onBeforeRequest.addListener(
     function(details) {
-        console.log(details);
-    },
-    {urls: ["https://nhcares.alayacare.com/#/scheduling/scheduled_visits"]}
-);
+        var filter = browser.webRequest.filterResponseData(details.requestId);
 
-console.log("Listener initiated");
+        let decoder = new TextDecoder("utf-8");
+        let encoder = new TextEncoder();
+        var obj = [];
+
+        filter.ondata = (event) => {
+            let str = decoder.decode(event.data, { stream: true });
+            obj.push(str);
+            filter.write(event.data);
+        };
+
+        filter.onstop = (event) => {
+            obj = obj.toString();
+            
+            console.log(obj);
+            var arr = JSON.parse(obj);
+            console.log(arr);
+
+            filter.close();
+          };
+    },
+    {urls: 
+        ["https://nhcares.alayacare.com/api/v1/scheduler/scheduled_visits*"]
+    },
+    ['blocking']
+);
