07e122acba13a0759290e27a4c40b3b2fa814c3a
tensesailor <69921413+tensesailor@users.noreply.github.com>
2025-01-26 19:16:20 -0800

    temp

--- test-extension/serviceworkers/service-worker.js
+++ test-extension/serviceworkers/service-worker.js
@@ -3,7 +3,7 @@ console.log("Service worker is running");
 chrome.webRequest.onCompleted.addListener(
     function(details) {
         console.log("Request intercepted:", details.url);
-        // You can add additional logic here if needed
+        
     },
     { urls: ["https://nhcares.alayacare.com/api/v1/scheduler/scheduled_visits*"] }
 );
