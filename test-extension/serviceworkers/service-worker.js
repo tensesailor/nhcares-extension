3c535136ea67039175fad334e938af9c79ae0dcf
tensesailor <69921413+tensesailor@users.noreply.github.com>
2025-01-26 20:09:06 -0800

    Update

--- test-extension/serviceworkers/service-worker.js
+++ test-extension/serviceworkers/service-worker.js
@@ -2,8 +2,21 @@ console.log("Service worker is running");
 
 chrome.webRequest.onCompleted.addListener(
     function(details) {
-        console.log("Request intercepted:", details.url);
-        
+        if (details.initiator === "https://nhcares.alayacare.com") {
+            let items = copyRequest(details.url);
+        }
     },
     { urls: ["https://nhcares.alayacare.com/api/v1/scheduler/scheduled_visits*"] }
 );
+
+function copyRequest(url) {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log("Fetched data:", data.items);
+            return data;
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+}
