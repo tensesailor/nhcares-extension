5428d2313bcb104569d59cd02545723f3828fd39
tensesailor <69921413+tensesailor@users.noreply.github.com>
2025-01-26 20:49:33 -0800

    fetch clients is functioning

--- test-extension/serviceworkers/service-worker.js
+++ test-extension/serviceworkers/service-worker.js
@@ -3,20 +3,19 @@ console.log("Service worker is running");
 chrome.webRequest.onCompleted.addListener(
     function(details) {
         if (details.initiator === "https://nhcares.alayacare.com") {
-            let items = copyRequest(details.url);
+            let visits = fetch(details.url)
+            .then(response => response.json())
+            .then(data => {
+                for (var i in data.items) {
+                    let test = fetch(`https://nhcares.alayacare.com/api/v2/patients/clients/${data.items[i].client.id}`)
+                    .then(clientresponse => clientresponse.json())
+                    .then(clientdata => console.log(clientdata))
+                    .catch(clienterror => console.error("Error fetching client:", clienterror));
+                };
+            })
+            .catch(error => console.error("Error fetching visits:", error));
         }
     },
     { urls: ["https://nhcares.alayacare.com/api/v1/scheduler/scheduled_visits*"] }
 );
 
-function copyRequest(url) {
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log("Fetched data:", data.items);
-            return data;
-        })
-        .catch(error => {
-            console.error("Error fetching data:", error);
-        });
-}
