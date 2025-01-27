01e74482ef0ddc867b5fada7c92cb410b63d4abf
Mitch <69921413+tensesailor@users.noreply.github.com>
2025-01-26 18:33:07 -0800

    initial transition to chrome

--- test-extension/contentscripts/fetchextra.js
+++ test-extension/contentscripts/fetchextra.js
@@ -1,7 +1,7 @@
-browser.runtime.onMessage.addListener( (message) => {
+chrome.runtime.onMessage.addListener( (message) => {
     
     if ((document.evaluate(`//span[text()="New Filters"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue)){
-        document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentElement.parentElement.querySelector(".datatable-column___facility").firstChild.innerHTML = message[1].demographics.city;// + " " + message[1].demographics.zip;
+        //document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentElement.parentElement.querySelector(".datatable-column___facility").firstChild.innerHTML = message[1].demographics.city;// + " " + message[1].demographics.zip;
     }
 
     else {
--- test-extension/serviceworkers/service-worker.js
+++ test-extension/serviceworkers/service-worker.js
@@ -1,48 +1,9 @@
-browser.webRequest.onBeforeRequest.addListener(
-    (details) => {
-        console.log("Request incoming", details.url);
-        let filter = browser.webRequest.filterResponseData(details.requestId);
+console.log("Service worker is running");
 
-        let decoder = new TextDecoder("utf-8");
-        var obj = "";
-
-        filter.ondata = (event) => {
-            console.log("Data received");
-            let str = decoder.decode(event.data, { stream: true });
-            obj += str;
-            filter.write(event.data);
-        };
-
-        filter.onstop = () => {
-            console.log("Data stream end");
-            
-            const clients = JSON.parse(obj).items;
-            
-            for (var i in clients) {
-                (function(id) {
-                    let req = new XMLHttpRequest();
-                    req.onload = () => {
-                        let extraData = JSON.parse(req.responseText);
-                        
-                        chrome.tabs.query({active: true, currentWindow: true}).then((tabs) => {
-                            console.log("Sending message to tab");
-                                chrome.tabs.sendMessage(tabs[0].id, [id, extraData]);
-                        })
-                    };
-                    req.open("GET", "https://nhcares.alayacare.com/api/v2/patients/clients/" + clients[i].client.id);
-                    req.send();
-                })(clients[i].id);
-            };
-
-            filter.close();
-            console.log("Filter closed");
-          };
+chrome.webRequest.onCompleted.addListener(
+    function(details) {
+        console.log("Request intercepted:", details.url);
+        // You can add additional logic here if needed
     },
-    {urls: 
-        [
-            "https://nhcares.alayacare.com/api/v1/scheduler/scheduled_visits*",
-            "https://nhcares.alayacare.com/api/v1/patients/clients*"
-        ]
-    },
-    ['blocking']
+    { urls: ["https://nhcares.alayacare.com/api/v1/scheduler/scheduled_visits*"] }
 );
