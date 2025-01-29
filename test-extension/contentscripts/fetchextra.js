5cc9e92d8caf551932bf31e1f5d7f0f1702341c7
Mitch <69921413+tensesailor@users.noreply.github.com>
2025-01-29 15:16:38 -0800

    cardinals and security improvements

--- test-extension/contentscripts/fetchextra.js
+++ test-extension/contentscripts/fetchextra.js
@@ -1,47 +1,13 @@
-console.log("Content script is running");
-
 chrome.runtime.onMessage.addListener( (message) => {
 
     if (document.evaluate(`//span[text()="New Filters"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue){
-        document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentElement.parentElement.querySelector(".datatable-column___facility").firstChild.innerHTML = message[1].demographics.city;// + " " + message[1].demographics.zip;
-        let visibletags = document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentElement.parentElement.querySelector(".datatable-column___approval_status").firstChild.firstChild;
-        if (message[1].tags.includes("Male Caregiver")){
-            if (message[1].tags.includes("Female Caregiver")){
-                visibletags.innerHTML = "No CG Preference";
-            } else {
-                visibletags.innerHTML = "Male CG Only";
-            };
-        } else if (message[1].tags.includes("Female Caregiver")){
-            visibletags.innerHTML = "Female CG Only";
-        } else {
-            visibletags.innerHTML = "No CG Preference";
-        };
-
-        let vital = message[1].tags.findIndex(element => element.includes("Vital"));
-        if (vital > -1){
-            visibletags.innerHTML += ", " + message[1].tags[vital];
-        };
+        document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentElement.parentElement.querySelector(".datatable-column___facility").firstChild.innerText = message[1];
+        document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText = message[2];
     }
 
     else {
-        document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentElement.parentElement.children[3].firstElementChild.innerHTML = message[1].demographics.city;// + " " + message[1].demographics.zip;
-        let visibletags = document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentElement.parentElement.children[9];
-        if (message[1].tags.includes("Male Caregiver")){
-            if (message[1].tags.includes("Female Caregiver")){
-                visibletags.innerHTML = "No CG Preference";
-            } else {
-                visibletags.innerHTML = "Male CG Only";
-            };
-        } else if (message[1].tags.includes("Female Caregiver")){
-            visibletags.innerHTML = "Female CG Only";
-        } else {
-            visibletags.innerHTML = "No CG Preference";
-        };
-
-        let vital = message[1].tags.findIndex(element => element.includes("Vital"));
-        if (vital > -1){
-            visibletags.innerHTML += ", " + message[1].tags[vital];
-        };
+        document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentElement.parentElement.children[3].firstElementChild.innerText = message[1];
+        document.evaluate(`//a[text()="${message[0].toString()}"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText = message[2];
     };
 
     return Promise.resolve();
--- test-extension/serviceworkers/service-worker.js
+++ test-extension/serviceworkers/service-worker.js
@@ -1,9 +1,6 @@
-console.log("Service worker is running");
-
 chrome.webRequest.onCompleted.addListener(
     function(details) {
         if (details.initiator === "https://nhcares.alayacare.com") {
-            console.log(details);
             let tab = details.tabId;
             fetch(details.url)
             .then(response => response.json())
@@ -11,8 +8,50 @@ chrome.webRequest.onCompleted.addListener(
                 for (let i in data.items) {
                     fetch(`https://nhcares.alayacare.com/api/v2/patients/clients/${data.items[i].client.id}`)
                     .then(clientresponse => clientresponse.json())
-                    .then(clientdata => chrome.tabs.sendMessage(tab, [data.items[i].id, clientdata]))
-                    .catch(clienterror => console.error("Error fetching client:", clienterror));
+                    .then(clientdata => {
+
+                        let location;
+
+                        if (clientdata.demographics.city === "Portland"){
+                            let hasdirection = false;
+                            let cardinals = [" N", " NE", " E", " SE", " S", " SW", " W", " NW"];
+                            for (let str in cardinals){
+                                if (clientdata.demographics.address.includes(cardinals[str] + " ") || clientdata.demographics.address.includes(cardinals[str] + ". ")){
+                                    location = cardinals[str] + " PDX";
+                                    hasdirection = true;
+                                    break;
+                                };
+                            };
+                            if (!hasdirection){
+                                location = "PDX";
+                            };
+                        } else {
+                            location = clientdata.demographics.city;
+                        };
+                    
+                        let visibletags;
+
+                        if (clientdata.tags.includes("Male Caregiver")){
+                            if (clientdata.tags.includes("Female Caregiver")){
+                                visibletags = "No CG Preference";
+                            } else {
+                                visibletags = "Male CG Only";
+                            };
+                        } else if (clientdata.tags.includes("Female Caregiver")){
+                            visibletags = "Female CG Only";
+                        } else {
+                            visibletags = "No CG Preference";
+                        };
+
+                        let vital = clientdata.tags.findIndex(element => element.includes("Vital"));
+                        if (vital > -1){
+                            visibletags += ", " + clientdata.tags[vital];
+                        };
+
+                        chrome.tabs.sendMessage(tab, [data.items[i].id, location, visibletags]);
+
+                    })
+                    .catch(clienterror => console.error("Error fetching client or sending data to content-script:", clienterror));
                 };
             })
             .catch(error => console.error("Error fetching visits:", error));
