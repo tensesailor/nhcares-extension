f24558e717e248370db41f177cca84d4d336604c
Mitch <69921413+tensesailor@users.noreply.github.com>
2025-05-21 10:59:40 -0700

    Delete installer.ps1

--- installer.ps1
+++
@@ -1,7 +0,0 @@
-Invoke-WebRequest -Uri "https://github.com/tensesailor/nhcares-extension/archive/refs/heads/main.zip" -OutFile "$([Environment]::GetFolderPath('UserProfile'))\Downloads\test.zip"
-
-Expand-Archive -Path "$([Environment]::GetFolderPath('UserProfile'))\Downloads\test.zip" -DestinationPath "$([Environment]::GetFolderPath('UserProfile'))\Documents" -Force
-
-Remove-Item -Path "$([Environment]::GetFolderPath('UserProfile'))\Downloads\test.zip" -Recurse
-
-Rename-Item -Path "$([Environment]::GetFolderPath('UserProfile'))\Documents\nhcares-extension-main" -NewName "test-extension" -Force
